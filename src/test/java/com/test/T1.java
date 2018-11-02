5640a4e9ba135b5aa13083990dc9559a0072ddf1
John Doe <john@sample.com>
2018-11-02 17:13:27 +0800

    调通流程。。

--- src/test/java/com/test/T1.java
+++ src/test/java/com/test/T1.java
@@ -4,8 +4,10 @@ import actions.Actions;
 import driverss.seleniumdriver;
 import findelement.Element;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -18,7 +20,7 @@ import static driverss.seleniumdriver.driver;
  */
 
 //
-public class T1 {
+public class T1{
     @BeforeTest
     public static void   open(){
         seleniumdriver.open("chrome");
@@ -40,7 +42,6 @@ public class T1 {
         Element.findelement(By.xpath("//*[@data-id=\"1\"]")).click();
         Element.findelement(By.linkText("费用申请")).click();
         Element.findelement(By.linkText("出差申请")).click();
-        //跳转到出差申请界面iframe
         WebElement fram=Element.findelement(By.id("list_228003"));
         driver.switchTo().frame(fram);
         Thread.sleep(6000);
@@ -52,7 +53,7 @@ public class T1 {
         //跳出iframe
         driver.switchTo().defaultContent();
         Thread.sleep(3000);
-        //进入新增出差申请界面iframe
+        //跳转到新增出差申请界面
         WebElement fram2=Element.findelement(By.xpath("//div[@class=\"layui-tab-item layui-show\"]/iframe[1]"));
         driver.switchTo().frame(fram2);
         Thread.sleep(3000);
@@ -63,8 +64,8 @@ public class T1 {
         //勾选上“是否借款”按钮
         WebElement anniu= Element.findelement(By.id("isborrow_228003"));
         anniu.click();
-        //Boolean a=anniu.isSelected();
-        //Assert.assertTrue(a);
+        Boolean a=anniu.isSelected();
+        Assert.assertTrue(a);
         //填写申请事由
         Element.findelement(By.id("memo_228003")).sendKeys("回家过年");
         //子表点击“新增”按钮
@@ -80,7 +81,7 @@ public class T1 {
         Element.findelement(By.id("okRight")).click();
         driver.switchTo().defaultContent();
         // 选择出发地
-        // 跳转到出差申请界面iframe
+        //跳转到出差申请界面
         driver.switchTo().frame(fram2);
         Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[7]")).click();
         Element.findelement(By.xpath("//span[@class=\"k-widget k-header\"]/span/span/span")).click();
@@ -91,7 +92,7 @@ public class T1 {
         Element.确定按钮();
         driver.switchTo().defaultContent();
         //选择目的地
-        //跳转到出差申请界面iframe
+        //跳转到出差申请界面
         driver.switchTo().frame(fram2);
         Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody//td[8]")).click();
         Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody//td[8]/span//span[2]//span")).click();
@@ -102,11 +103,46 @@ public class T1 {
         Element.确定按钮();
         driver.switchTo().defaultContent();
         //选择出发日期
-        //跳转到出差申请界面iframe
+        //跳转到出差申请界面
         driver.switchTo().frame(fram2);
-      //  Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table//tr/td[9]/span//span//input")).click();
-        //测试拖拽
-        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table//tr/td[15]/span/span/span/input")).click();
+        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[9]")).click();
+        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[9]/span/span/span/span")).click();
+        driver.switchTo().defaultContent();
+        WebElement frame6=Element.findelement(By.xpath("//*[@class=\"layui-tab-item layui-show\"]/iframe"));
+        driver.switchTo().frame(frame6);
+        Element.findelement(By.linkText("14")).click();
+        driver.switchTo().defaultContent();
+        //返程日期
+        driver.switchTo().frame(fram2);
+        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[10]")).click();
+        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[10]/span/span/span/span")).click();
+        driver.switchTo().defaultContent();
+        WebElement frame7=Element.findelement(By.xpath("//*[@class=\"layui-tab-item layui-show\"]/iframe"));
+        driver.switchTo().frame(frame7);
+        Element.findelement(By.linkText("16")).click();
+        driver.switchTo().defaultContent();
+        //交通工具
+        driver.switchTo().frame(fram2);
+        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[11]")).click();
+        Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]/table/tbody/tr/td[11]/span/span/span/span")).click();
+        driver.switchTo().defaultContent();
+        WebElement frame8=Element.findelement(By.xpath("//*[@class=\"layui-tab-item layui-show\"]/iframe"));
+        driver.switchTo().frame(frame8);
+        Element.findelement(By.id("train")).click();
+
+        //定位到的元素可以使用下列方法
+       // WebElement a=Element.findelement(By.xpath("//*[@id=\"grid_22800301\"]/div[2]"));
+//        org.openqa.selenium.interactions.Actions actions =new org.openqa.selenium.interactions.Actions(driver);
+//        actions.dragAndDropBy(a,1000,1000).perform();
+//        Thread.sleep(4000);
+       //通过js实现拖拽操作（定位不到的元素）
+        org.openqa.selenium.interactions.Actions actions =new org.openqa.selenium.interactions.Actions(driver);
+//        actions.moveToElement(a,444,465).perform();
+        JavascriptExecutor js =(JavascriptExecutor)driver;
+       js.executeScript(" document.getElementsByClassName(\"k-grid-content k-auto-scrollable\")[0].scrollTo(800,0)");
+        Thread.sleep(4000);
+
+
 
 
 
